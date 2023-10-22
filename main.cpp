70359a37e8e4d19f63f360d6d86a0721e07abcee
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-10-22 11:18:25 +0300

    ex-2 was done

--- main.cpp
+++ main.cpp
@@ -4,5 +4,19 @@ using namespace std;
 
 int main()
 {
+	int n, k;
 
+	std::cin >> n >> k;
+
+	if (n < k)
+
+		std::cout << "first is less";
+
+	else if (n > k)
+
+		std::cout << "second is less";
+
+	else
+
+		std::cout << "equal";
 }
