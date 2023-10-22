b36a6db65a330a5c0b211067b97ad2933e8400a1
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-10-22 11:32:27 +0300

    ex-4 was done

--- main.cpp
+++ main.cpp
@@ -4,5 +4,23 @@ using namespace std;
 
 int main()
 {
+    int n, k;
 
+    std::cin >> n >> k;
+
+    if (n == k)
+
+        std::cout << "equal\n" << n << ' ' << k;
+
+    else {
+
+        std::cout << "not equal\n";
+
+        if (n > k)
+
+            std::cout << k << ' ' << n;
+
+        else
+
+            std::cout << n << ' ' << k;
 }
