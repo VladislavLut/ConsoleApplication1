16c4b6eaf417264bb5d9af2ed61c66b7d7f9f812
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-10-22 11:20:53 +0300

    ex - 3 was done

--- main.cpp
+++ main.cpp
@@ -4,5 +4,19 @@ using namespace std;
 
 int main()
 {
+	int n;
 
+	std::cin >> n;
+
+	if (n > 0)
+
+		std::cout << "positive";
+
+	else if (n < 0)
+
+		std::cout << "negative";
+
+	else
+
+		std::cout << "zero";
 }
