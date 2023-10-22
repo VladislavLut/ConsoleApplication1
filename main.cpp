66d5413a15375d95b200b02687eb886ea4535b25
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-10-22 11:21:50 +0300

    initial commit

--- main.cpp
+++ main.cpp
@@ -4,19 +4,5 @@ using namespace std;
 
 int main()
 {
-	int n;
 
-	std::cin >> n;
-
-	if (n > 0)
-
-		std::cout << "positive";
-
-	else if (n < 0)
-
-		std::cout << "negative";
-
-	else
-
-		std::cout << "zero";
 }
