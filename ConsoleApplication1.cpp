af2af051ff68266221dff427c3d3d545982bd914
Lutsenko Vladyslav Serhiyovych <143324701+VladislavLut@users.noreply.github.com>
2023-10-30 11:07:14 +0200

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -2,9 +2,25 @@
 
 using namespace std;
 
-int main()
-{
-	int n;
+int findMaximum() {
+	int maxNumber = INT_MIN;
 
-	cin >>(n % 2 )
+	for (int i = 0; i < 7; i++) {
+		int number;
+		cout << "Enter number " << i + 1 << " : ";
+		cin >> number;
+
+		if (number > maxNumber) {
+			maxNumber = number;
+
+		}
+
+	}
+	return maxNumber;
+}
+int main() {
+	int maximum = findMaximum();
+	cout << "The maximum number is: " << maximum << endl;
+
+	return 0;
 }
