ed670850d7b53124566293a1b1ebcee97a64ca9b
Lutsenko Vladyslav Serhiyovych <143324701+VladislavLut@users.noreply.github.com>
2023-10-30 10:42:56 +0200

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main()
-{
-	int n;
+int swapDigits(int number) {
+	string numberString = to_string(number);
 
-	cin >>(n % 2 )
+	if (numberString.length() != 4) {
+		throw invalid_argument("Input should be a four-digit number.");
+	}
+
+	swap(numberString[0], numberString[1]);
+	swap(numberString[2], numberString[3]);
+
+	int modifiedNumber = stoi(numberString);
+
+	return modifiedNumber;
+
+
+}
+int main() {
+	int number;
+	cout << "Enter a four-digit number: ";
+	cin >> number;
+
+	try {
+		int modifiedNumber = swapDigits(number);
+		cout << "Modified number: " << modifiedNumber << endl;
+	}
+	catch (const invalid_argument& e) {
+		cout << "Error: " << e.what() << endl;
+	}
+	return 0;
 }
